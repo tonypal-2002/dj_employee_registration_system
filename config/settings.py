5c4afbde182626bf5448b90d9836f20c5da868b0
tonypal-2002 <tonypal2002@gmail.com>
2024-08-20 12:33:38 +0530

    api_v1 added to settings.py

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'backend'
+    'backend',
+    'api_v1'
 ]
 
 MIDDLEWARE = [
